bc56e8a62c91f734621462b7c9779b9a6d7da496
ralstonb9 <ralstonb9@gmail.com>
2023-11-06 22:15:00 -0800

    fixed minor mistakes and began testing routes

--- models/Comment.js
+++ models/Comment.js
@@ -1,7 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Comment extends model {}
+class Comment extends Model {}
 
 Comment.init(
     {
